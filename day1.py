dd48fd16bb4674d5fce3988beeccba6bb5f28ca1
Meithan West <meithan@gmail.com>
2019-12-01 14:55:14 -0600

    Day 1 solution

---
+++ day1.py
@@ -0,0 +1,26 @@
+from math import floor
+
+def compute_fuel(mass):
+  return floor(mass/3) - 2
+
+def compute_fuel_part2(mass):
+  tot_fuel = compute_fuel(mass)
+  mass = tot_fuel
+  while True:
+    fuel = compute_fuel(mass)
+    if fuel <= 0: break
+    tot_fuel += fuel
+    mass = fuel
+  return tot_fuel
+
+masses = []
+with open("day1.in") as f:
+  for line in f:
+    masses.append(int(line))
+
+# Part 1
+total_fuel_1 = sum([compute_fuel(m) for m in masses])
+print(f"Part 1: {total_fuel_1}")
+
+total_fuel_2 = sum([compute_fuel_part2(m) for m in masses])
+print(f"Part 2: {total_fuel_2}")
