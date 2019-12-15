75137eabed145629cf1bc105b2fc57f0ae5a299a
Meithan West <meithan@gmail.com>
2019-12-15 13:25:47 -0600

    Move IntcodeComputer to separate file
    Change previous solutions to use common code

---
+++ IntcodeComputer.py
@@ -0,0 +1,147 @@
+from queue import Queue
+from collections import defaultdict
+
+class IntcodeComputer:
+
+  def __init__(self, program, inputs=[]):
+    self.memory = defaultdict(lambda: 0)
+    for i in range(len(program)):
+      self.memory[i] = program[i]
+    self.inputs = Queue()
+    for item in inputs:
+      self.inputs.put(item)
+    self.outputs = []
+    self.last_output = None
+    self.ptr = 0
+    self.relbase = 0
+    self.halted = False
+
+  def deref_params(self, params, modes, num):
+    for i in range(num):
+      if modes[i] == 0:
+        params[i] = self.memory[params[i]]
+      elif modes[i] == 1:
+        pass
+      elif modes[i] == 2:
+        params[i] = self.memory[self.relbase + params[i]]
+    return params
+
+  def execute(self, quiet=False, pause_on_output=False):
+
+    while True:
+
+      instruction = self.memory[self.ptr]
+      opcode, nargs, modes = self.decode(instruction)
+      # print("ptr=",self.ptr,"instruction=",instruction)
+
+      if opcode == 99:
+        self.halted = True
+        return
+
+      # Read parameters
+      params = [self.memory[self.ptr+i] for i in range(1,nargs+1)]
+
+      # ADD
+      if opcode == 1:
+        params = self.deref_params(params, modes, 2)
+        if modes[2] == 0: addr = params[2]
+        elif modes[2] == 2: addr = self.relbase + params[2]
+        self.memory[addr] = params[0] + params[1]
+        self.ptr += nargs + 1
+
+      # MULT
+      elif opcode == 2:
+        params = self.deref_params(params, modes, 2)
+        if modes[2] == 0: addr = params[2]
+        elif modes[2] == 2: addr = self.relbase + params[2]
+        self.memory[addr] = params[0] * params[1]
+        self.ptr += nargs + 1
+
+      # INPUT
+      elif opcode == 3:
+        if modes[0] == 0:
+          addr = params[0]
+        elif modes[0] == 2:
+          addr = self.relbase + params[0]
+        if not self.inputs.empty():
+          next_input = self.inputs.get()
+          if not quiet:
+            print("input>", next_input)
+          self.memory[addr] = next_input
+          next_input += 1
+        else:
+          answer = int(input("input> "))
+          self.memory[addr] = int(answer)
+        self.ptr += nargs + 1
+
+      # OUTPUT
+      elif opcode == 4:
+        params = self.deref_params(params, modes, 1)
+        if not quiet:
+          print(f"output> {params[0]}")
+        self.outputs.append(params[0])
+        self.last_output = params[0]
+        self.ptr += nargs + 1
+        if pause_on_output:
+          return
+
+      # JUMP_TRUE
+      elif opcode == 5:
+        params = self.deref_params(params, modes, 2)
+        if params[0] != 0:
+          self.ptr = params[1]
+        else:
+          self.ptr += nargs + 1
+
+      # JUMP_FALSE
+      elif opcode == 6:
+        params = self.deref_params(params, modes, 2)
+        if params[0] == 0:
+          self.ptr = params[1]
+        else:
+          self.ptr += nargs + 1
+
+      # LESS_THAN
+      elif opcode == 7:
+        params = self.deref_params(params, modes, 2)
+        if modes[2] == 0: addr = params[2]
+        elif modes[2] == 2: addr = self.relbase + params[2]
+        self.memory[addr] = 1 if params[0] < params[1] else 0
+        self.ptr += nargs + 1
+
+      # EQUAL
+      elif opcode == 8:
+        params = self.deref_params(params, modes, 2)
+        if modes[2] == 0: addr = params[2]
+        elif modes[2] == 2: addr = self.relbase + params[2]
+        self.memory[addr] = 1 if params[0] == params[1] else 0
+        self.ptr += nargs + 1
+
+      # REL_OFFSET
+      elif opcode == 9:
+        params = self.deref_params(params, modes, 1)
+        self.relbase += params[0]
+        self.ptr += nargs + 1
+
+  def decode(self, instruction):
+
+    opcode = instruction % 100
+
+    if opcode == 1: nargs = 3
+    elif opcode == 2: nargs = 3
+    elif opcode == 3: nargs = 1
+    elif opcode == 4: nargs = 1
+    elif opcode == 5: nargs = 2
+    elif opcode == 6: nargs = 2
+    elif opcode == 7: nargs = 3
+    elif opcode == 8: nargs = 3
+    elif opcode == 9: nargs = 1
+    elif opcode == 99: nargs = 0
+
+    modes = []
+    m = instruction // 100
+    for i in range(nargs):
+      modes.append(m % 10)
+      m = m // 10
+
+    return (opcode, nargs, modes)
--- day2.py
+++ day2.py
@@ -1,30 +1,6 @@
 import sys
 
-def run_program(program):
-
-  memory = program[:]
-  ptr = 0
-  opcode = memory[0]
-
-  while opcode != 99:
-
-    ptr_A = memory[ptr+1]
-    ptr_B = memory[ptr+2]
-    ptr_C = memory[ptr+3]
-    val_A = memory[ptr_A]
-    val_B = memory[ptr_B]
-
-    if opcode == 1:
-      result = val_A + val_B
-    elif opcode == 2:
-      result = val_A * val_B
-
-    memory[ptr_C] = result
-
-    ptr += 4
-    opcode = memory[ptr]
-
-  return memory
+from IntcodeComputer import IntcodeComputer
 
 # Load input
 with open(sys.argv[1]) as f:
@@ -36,24 +12,22 @@ with open(sys.argv[1]) as f:
 program[1] = 12
 program[2] = 2
 
-final_mem = run_program(program)
-result = final_mem[0]
-print(f"Part 1: {result}")
+computer = IntcodeComputer(program)
+computer.execute()
+
+print(f"Part 1: {computer.memory[0]}")
 
 # Part 2
 
 for A in range(0,100):
   for B in range(0,100):
-  
+
     program[1] = A
     program[2] = B
-    
-    final_mem = run_program(program)
-    result = final_mem[0]
-    
+
+    computer = IntcodeComputer(program)
+    computer.execute()
+    result = computer.memory[0]
+
     if result == 19690720:
-    
-
-      #print(A, B, result)
       print(f"Part 2: {100*A + B}")
-
